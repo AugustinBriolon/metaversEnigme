fe0067b388719692b2e93632b6d2fdbf7642cc4c
Louis Nourry <louinourry@hotmail.fr>
2022-11-17 14:34:58 +0100

    game3update

--- src/components/Game3/Questions.js
+++ src/components/Game3/Questions.js
@@ -1,4 +1,4 @@
-import imgMcDo from '../../assets/img/mcdo.png'
+import imgInDesign from '../../assets/img/indesign_pixel.png'
 import imgPhotoshop from '../../assets/img/psd.png'
 import imgIllustrator from '../../assets/img/illustator.png'
 
@@ -17,12 +17,12 @@ export const questions = [
     },
     {
       questionText: "A quelle entreprise correspond ce logo ?",
-      questionImage: imgMcDo,
+      questionImage: imgInDesign,
       answerOptions: [
-        { answerText: "IIM", isCorrect: false },
-        { answerText: "McDo", isCorrect: true },
-        { answerText: "EMLV", isCorrect: false },
-        { answerText: "ESILV", isCorrect: false },
+        { answerText: "Google Agenda", isCorrect: false },
+        { answerText: "Adobe InDesign", isCorrect: true },
+        { answerText: "Trello", isCorrect: false },
+        { answerText: "Mailjet", isCorrect: false },
       ],
     },
     {
